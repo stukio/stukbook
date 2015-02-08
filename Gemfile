cefd6609015fa210d3f3a2826739117a3b5c4de4
Tiago Martins <tiagom87@gmailc.om>
2015-02-08 21:03:33 +0000

    Added custom version of devise

--- Gemfile
+++ Gemfile
@@ -47,3 +47,5 @@ end
 gem 'twitter-bootstrap-rails', :git => 'git://github.com/seyhunak/twitter-bootstrap-rails.git'
 
 gem 'simple_form'
+
+gem 'devise'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Stukbook::Application.routes.draw do
+  devise_for :users, controllers: {registrations: 'registrations'}
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
