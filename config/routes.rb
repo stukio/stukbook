219d23c04e45a32ae9d41e84534209c18d1ec4b7
Tiago Martins <tiagom87@gmailc.om>
2015-02-22 23:41:16 +0000

    Added a user profile

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Stukbook::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'pages#home'
 
+  resources :users, only: [:show]
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
