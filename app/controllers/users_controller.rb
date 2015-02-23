d77a8feb0b12792214a21d53e4e75690a32fe647
Tiago Martins <tiagom87@gmailc.om>
2015-02-23 14:39:15 +0000

    Added users index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 	before_action :set_user, only:[:show]
+
+	def index
+		@users = User.all
+	end
+
   def show
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Stukbook::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'pages#home'
 
-  resources :users, only: [:show]
+  resources :users, only: [:show, :index]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
