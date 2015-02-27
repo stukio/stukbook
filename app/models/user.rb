893e1425fc9e89699b733f390f12b5f1a940bed4
Tiago Martins <tiagom87@gmailc.om>
2015-02-27 16:31:34 +0000

    Added friendship types

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,16 @@ class User < ActiveRecord::Base
   	self.friendships.create(friend: user_2)
   end
 
+  def pending_friend_requests_from
+    self.inverse_friendships.where(state: "pending")
+  end
+
+  def pending_friend_requests_to
+    self.friendships.where(state: "pending")
+  end
+
+  def active_friends
+    self.friendships.where(state: "active").map(&:friend) + self.inverse_friendships.where(state: "active").map(&:user)
+  end
+
 end
