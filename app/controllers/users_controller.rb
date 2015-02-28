baaddf032fdbf6108543840b08e22265574e4cbb
Tiago Martins <tiagom87@gmailc.om>
2015-02-28 21:46:27 +0000

    Added user detection

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,17 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only:[:index]
 	before_action :set_user, only:[:show]
 
 	def index
-		@users = User.all
+		case params[:people] when "friends"
+      @users = current_user.active_friends
+    when "requests"
+      @users = current_user.pending_friend_requests_from.map(&:user)
+    when "pending"
+      @users = current_user.pending_friend_requests_to.map(&:friend)
+    else
+      @users = User.where.not(id: current_user.id)
+    end
 	end
 
   def show
