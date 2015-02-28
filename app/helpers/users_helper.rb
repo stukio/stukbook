d1d4d61c6c4f9a0730d892af7c5ba7337efa6f48
Tiago Martins <tiagom87@gmailc.om>
2015-02-28 18:31:37 +0000

    Added friendship status detection

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+	def action_buttons(user)
+		case current_user.friendship_status(user) when "friends"
+			"Remove Friendship"
+		when "pending"
+			"Cancel Request"
+		when "requested"
+			"Accept or Deny"
+		when "not_friends"
+			"Add as a Friend"
+		end
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -27,4 +27,20 @@ class User < ActiveRecord::Base
     self.friendships.where(state: "active").map(&:friend) + self.inverse_friendships.where(state: "active").map(&:user)
   end
 
+  def friendship_status(user_2)
+    friendship = Friendship.where(user_id: [self.id,user_2.id], friend_id: [self.id,user_2.id])
+    unless friendship.any?
+      return "not_friends"
+    else
+      if friendship.first.state == "active"
+        return "friends"
+      else
+        if friendship.first.user == self
+          return "pending"
+        else
+          return "requested"
+        end
+      end
+    end
+  end
 end
